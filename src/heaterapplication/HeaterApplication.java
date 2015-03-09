134a042fcfaa34f8fae3e341e1681296fd3e9df0
Stefan <stefan.ramilli@gmail.com>
2015-03-09 22:15:36 +0100

    first commit

---
+++ src/heaterapplication/HeaterApplication.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package heaterapplication;
+
+import javax.microedition.midlet.MIDlet;
+
+/**
+ *
+ * @author Ste
+ */
+public class HeaterApplication extends MIDlet {
+    
+    @Override
+    public void startApp() {
+    }
+    
+    @Override
+    public void destroyApp(boolean unconditional) {
+    }
+}
